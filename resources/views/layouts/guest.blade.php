356d709296ce97a2755748c393fcb5b592aacb8a
raihan-js <araihansikder@gmail.com>
2024-04-06 22:14:51 +0600

    build issue3

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -10,12 +10,12 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-        <link href="{{ asset('build/assets/app-Br4EDtGW.css') }}" rel="stylesheet">
-        <script src="{{ asset('build/assets/app-BMCYRmuW.js') }}"></script>
+        <link href="{{ secure_asset('build/assets/app-Br4EDtGW.css') }}" rel="stylesheet">
+        <script src="{{ secure_asset('build/assets/app-BMCYRmuW.js') }}"></script>
 
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
         @livewireStyles
     </head>
     <body class="font-sans text-gray-900 antialiased">
