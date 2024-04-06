74a31e01489d37ece74e6fa7f6e84e58c9368c89
raihan-js <araihansikder@gmail.com>
2024-04-06 21:27:58 +0600

    vite config error fix

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import path from 'path';
 
 export default defineConfig({
     plugins: [
@@ -11,4 +12,14 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    build: {
+        // Specify the output directory
+        outDir: path.resolve(__dirname, 'public/build'),
+    },
+    resolve: {
+        // Setup an alias if needed, this is optional
+        alias: {
+            '@': path.resolve(__dirname, 'resources/js'),
+        },
+    },
 });
