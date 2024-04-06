da01c97e259b6b304ec7c26932f1fe1267b6730b
raihan-js <araihansikder@gmail.com>
2024-04-06 22:49:32 +0600

    build issue6

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -10,7 +10,8 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
+        <link href="{{ secure_asset('build/assets/app-Br4EDtGW.css') }}" rel="stylesheet">
+        <script src="{{ secure_asset('build/assets/app-BMCYRmuW.js') }}"></script>
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         @livewireStyles
